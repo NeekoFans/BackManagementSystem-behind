ee731e5cfa6bf32886382d0131b3da29b46d83fa
田加丰 <1148774505@qq.com>
2023-07-18 10:11:06 +0800

    第一次修改

--- src/main/java/com/example/pico/PicoApplication.java
+++ src/main/java/com/example/pico/PicoApplication.java
@@ -3,10 +3,20 @@ package com.example.pico;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class PicoApplication {
 
+
     public static void main(String[] args) {
+        Map map=new HashMap();
+        map.put("id","9527");
+        map.put("name","张三");
+        map.put("age","20");
+        map.put("date",new Date());
         SpringApplication.run(PicoApplication.class, args);
     }
 
